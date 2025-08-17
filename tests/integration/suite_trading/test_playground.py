8df483d7f05c87b3a0b6e28083a29279e375f3bf
Stefan Simik <stefan.simik@gmail.com>
2025-08-17 03:00:11 +0200

    Update test playground: Add PeriodicTimeEventFeed into DemoStrategy
    Introduced a `PeriodicTimeEventFeed` in `DemoStrategy` to emit time tick events aligned with the existing bar feed timeline. Updated `DemoStrategy` to manage both bar and time feeds, including auto-stopping the time feed when the bar feed is finished. Enhanced event handling to process new time tick events.

--- tests/integration/suite_trading/test_playground.py
+++ tests/integration/suite_trading/test_playground.py
@@ -1,7 +1,9 @@
 import logging
+from datetime import timedelta
 from suite_trading.domain.market_data.bar.bar_event import NewBarEvent
 from suite_trading.platform.event_feed.demo_bar_event_feed import DemoBarEventFeed
 from suite_trading.platform.event_feed.event_feed import EventFeed
+from suite_trading.platform.event_feed.periodic_time_event_feed import PeriodicTimeEventFeed, TimeTickEvent
 from suite_trading.strategy.strategy import Strategy
 from suite_trading.platform.engine.trading_engine import TradingEngine
 
@@ -9,14 +11,35 @@ logger = logging.getLogger(__name__)
 
 
 class DemoStrategy(Strategy):
+    def __init__(self):
+        super().__init__()
+        self.bars_feed = None
+        self.time_feed = None
+
     def on_start(self):
         # Add market data via event-feed
-        demo_feed: EventFeed = DemoBarEventFeed()
-        self.add_event_feed("demo_feed", demo_feed, self.on_event)
+        bars_feed: EventFeed = DemoBarEventFeed(num_bars=20)
+        self.add_event_feed("bars_feed", bars_feed, self.on_event)
+        self.bars_feed = bars_feed  # Remember feed
+
+        # Add a periodic time event-feed aligned with bar feed timeline
+        start_dt_of_demo_bar_event_feed = bars_feed.peek().bar.end_dt
+        time_feed: EventFeed = PeriodicTimeEventFeed(
+            start_datetime=start_dt_of_demo_bar_event_feed,
+            interval=timedelta(seconds=10),
+            end_datetime=start_dt_of_demo_bar_event_feed + timedelta(minutes=20),
+        )
+        self.add_event_feed("time_feed", time_feed, self.on_event)
+        self.time_feed = time_feed  # Remember feed
 
     def on_event(self, event):
         if isinstance(event, NewBarEvent):
             self.on_bar(event.bar, event)
+            # Auto-stop time-feed, when bars-feed is finished
+            if self.bars_feed.is_finished():
+                self.time_feed.close()
+        elif isinstance(event, TimeTickEvent):
+            logger.debug(f"Received time tick: {event}")
         else:
             logger.debug(f"Received event: {event}")
 
