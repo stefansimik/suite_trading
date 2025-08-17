d95549a6833b3487da6f8e5cb3efa4e3665b1e6c
Stefan Simik <stefan.simik@gmail.com>
2025-08-18 00:10:27 +0200

    Update test playground: improve comments and logging for event feeds
    Enhanced clarity in `test_playground.py` by refining comments describing the usage of bar and time event feeds. Adjusted logging to better indicate unhandled events and removed redundant debug logs for time tick events. Simplified code where applicable.

--- tests/integration/suite_trading/test_playground.py
+++ tests/integration/suite_trading/test_playground.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from suite_trading.domain.market_data.bar.bar_event import NewBarEvent
 from suite_trading.platform.event_feed.demo_bar_event_feed import DemoBarEventFeed
 from suite_trading.platform.event_feed.event_feed import EventFeed
-from suite_trading.platform.event_feed.periodic_time_event_feed import PeriodicTimeEventFeed, TimeTickEvent
+from suite_trading.platform.event_feed.periodic_time_event_feed import PeriodicTimeEventFeed
 from suite_trading.strategy.strategy import Strategy
 from suite_trading.platform.engine.trading_engine import TradingEngine
 
@@ -17,12 +17,12 @@ class DemoStrategy(Strategy):
         self.time_feed = None
 
     def on_start(self):
-        # Add market data via event-feed
+        # Add data: 1-minute bars (demo data)
         bars_feed: EventFeed = DemoBarEventFeed(num_bars=20)
         self.add_event_feed("bars_feed", bars_feed, self.on_event)
         self.bars_feed = bars_feed  # Remember feed
 
-        # Add a periodic time event-feed aligned with bar feed timeline
+        # Add data: Time events each 10 seconds
         start_dt_of_demo_bar_event_feed = bars_feed.peek().bar.end_dt
         time_feed: EventFeed = PeriodicTimeEventFeed(
             start_datetime=start_dt_of_demo_bar_event_feed,
@@ -36,15 +36,11 @@ class DemoStrategy(Strategy):
     def on_event(self, event):
         if isinstance(event, NewBarEvent):
             self.on_bar(event.bar, event)
-        elif isinstance(event, TimeTickEvent):
-            logger.debug(f"Received time tick: {event}")
         else:
-            logger.debug(f"Received event: {event}")
+            logger.debug(f"Received (unhandled) event: {event}")
 
     def on_bar(self, bar, event: NewBarEvent):
         logger.debug(f"Received bar: {bar}")
-        # Time-feed will auto-finish when bars-feed finishes via $finish_with_feed.
-        pass
 
     def on_stop(self):
         pass
